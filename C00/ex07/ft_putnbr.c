04a994cb07d4f62aec7220d7370b9fff49eb6575
Hyan Guilherme aragao <hguilher@c2r4p1.42sp.org.br>
2023-03-20 18:28:32 -0300

    C03

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -6,11 +6,11 @@
 /*   By: hguilher <hguilher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 14:37:17 by hguilher          #+#    #+#             */
-/*   Updated: 2023/03/08 14:37:47 by hguilher         ###   ########.fr       */
+/*   Updated: 2023/03/20 17:23:17 by hguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include	<unistd.h>
+#include <unistd.h>
 
 void	ft_putnbr(int nb)
 {
@@ -20,16 +20,22 @@ void	ft_putnbr(int nb)
 	{
 		ft_putnbr(nb / 10);
 		h = (nb % 10 + '0');
-		write (1, &h, 1);
+		write(1, &h, 1);
 	}
 	else if (nb >= 0 && nb <= 9)
 	{
 		h = (nb % 10 + '0');
-		write (1, &h, 1);
+		write(1, &h, 1);
+	}
+	else if (nb == -2147483648)
+	{
+		write(1, "-", 1);
+		ft_putnbr(214748364);
+		write(1, "8", 1);
 	}
 	else
 	{
-		write (1, "-", 1);
+		write(1, "-", 1);
 		ft_putnbr(nb * -1);
 	}
 }
